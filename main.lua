5d891c625f1613d08cc2dae607e75031d27892f6
Henrique Rocha <hsrocha@loyola.edu>
2025-02-10 11:07:57 -0500

    Lecture 12 -- Adding Background class and bgs to main

--- main.lua
+++ main.lua
@@ -1,12 +1,17 @@
 local Globals = require "src.Globals"
 local Push = require "libs.push"
+local Background = require "src.game.Background"
 
 -- Load is executed only once; used to setup initial resource for your game
 function love.load()
-    love.window.setTitle("CS489 Title")
+    love.window.setTitle("CS489 Jewels")
     Push:setupScreen(gameWidth, gameHeight, windowWidth, windowHeight, {fullscreen = false, resizable = true})
     math.randomseed(os.time()) -- RNG setup for later
 
+    titleFont = love.graphics.newFont(32)
+
+    bg1 = Background("graphics/bg/background1.png",30)
+    bg2 = Background("graphics/bg/background2.png",60)
 end
 
 -- When the game window resizes
@@ -20,6 +25,8 @@ function love.keypressed(key)
         love.event.quit()
     elseif key == "F2" or key == "tab" then
         debugFlag = not debugFlag
+    elseif key == "return" and gameState=="start" then
+        gameState = "play"
     end
 end
 
@@ -31,14 +38,45 @@ end
 
 -- Update is executed each frame, dt is delta time (a fraction of a sec)
 function love.update(dt)
-
+    bg1:update(dt)
+    bg2:update(dt)
 end
 
 -- Draws the game after the update
 function love.draw()
     Push:start()
+
     -- always draw between Push:start() and Push:finish()
+    if gameState== "start" then
+        drawStartState()
+    elseif gameState == "play" then
+        drawPlayState()    
+    elseif gameState == "over" then
+        drawGameOverState()    
+    end
 
     Push:finish()
 end
 
+function drawStartState()
+    bg1:draw()
+    bg2:draw()
+
+    love.graphics.printf("CS489 Jewels",titleFont,0,50,
+        gameWidth,"center")
+    love.graphics.printf("Press Enter to Play or Escape to exit",
+        0,90, gameWidth,"center")
+    
+end
+
+function drawPlayState()
+    love.graphics.printf("Play",titleFont,0,50,
+    gameWidth,"center")
+end
+
+function drawGameOverState()
+    love.graphics.printf("GameOver",titleFont,0,50,
+        gameWidth,"center")
+    love.graphics.printf("Press Enter to Play or Escape to exit",
+        0,90, gameWidth,"center")
+end
---
+++ src/game/Background.lua
@@ -0,0 +1,21 @@
+local Class = require "libs.hump.class"
+
+local Background = Class{}
+function Background:init(imgPath, speed)
+    self.img = love.graphics.newImage(imgPath)
+    self.pos = 0
+    self.width = self.img:getWidth()
+    self.speed = speed
+end
+
+function Background:draw()
+    love.graphics.draw(self.img,0-self.pos,0)
+    love.graphics.draw(self.img,self.width-self.pos,0)    
+end
+
+function Background:update(dt)
+    self.pos = (self.pos+self.speed*dt)%self.width
+end
+
+
+return Background
